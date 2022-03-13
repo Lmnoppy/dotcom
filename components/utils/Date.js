156495299105c25696ba730b8cbebe73b55a7d03
lmnoppy <code@lmnoppy.com>
2022-03-13 16:36:37 +0000

    cleanup

--- components/utils/Date.js
+++
@@ -1,8 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export function sortByDateMostRecent(data) {
-  const sortedDate = data.sort(function (a, b) {
-    return new Date(b.date) - new Date(a.date);
-  })
-  return sortedDate;
-}
--- components/utils/Divider.js
+++
@@ -1,3 +0,0 @@
-export default function Divider(){
-    return <hr className="sidebar-hr opacity-25" />;
-} 
